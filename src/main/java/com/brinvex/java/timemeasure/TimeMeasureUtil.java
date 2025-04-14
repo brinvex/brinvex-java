d7a9f80f70a23342cf61861a9f1c81184047a56d
brinvex-dev1 <dev1@brinvex.com>
2025-04-14 22:19:30 +0200

    use System.nanoTime() in TimeMeasureUtil

--- src/main/java/com/brinvex/java/timemeasure/TimeMeasureUtil.java
+++ src/main/java/com/brinvex/java/timemeasure/TimeMeasureUtil.java
@@ -4,58 +4,57 @@ import com.brinvex.java.ThrowingRunnable;
 import com.brinvex.java.ThrowingSupplier;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.util.function.Supplier;
 
 public class TimeMeasureUtil {
 
     public static <T, E extends Exception> TimeMeasureResult<T> measureThrowing(ThrowingSupplier<T, E> task) throws TimeMeasureException {
-        Instant start = Instant.now();
+        long start = System.nanoTime();
         T result;
         try {
             result = task.get();
-            Instant end = Instant.now();
-            return new TimeMeasureResult<>(result, Duration.between(start, end));
+            long end = System.nanoTime();
+            return new TimeMeasureResult<>(result, Duration.ofNanos(end - start));
         } catch (Exception e) {
-            Instant end = Instant.now();
-            throw new TimeMeasureException(e, Duration.between(start, end));
+            long end = System.nanoTime();
+            throw new TimeMeasureException(e, Duration.ofNanos(end - start));
         }
     }
 
     public static <E extends Exception> Duration measureThrowing(ThrowingRunnable<E> task) throws TimeMeasureException {
-        Instant start = Instant.now();
+        long start = System.nanoTime();
         try {
             task.run();
-            Instant end = Instant.now();
-            return Duration.between(start, end);
+            long end = System.nanoTime();
+            return Duration.ofNanos(end - start);
         } catch (Exception e) {
-            Instant end = Instant.now();
-            throw new TimeMeasureException(e, Duration.between(start, end));
+            long end = System.nanoTime();
+            throw new TimeMeasureException(e, Duration.ofNanos(end - start));
         }
     }
 
     public static <T> TimeMeasureResult<T> measure(Supplier<T> task) {
-        Instant start = Instant.now();
+        long start = System.nanoTime();
         T result;
         try {
             result = task.get();
-            Instant end = Instant.now();
-            return new TimeMeasureResult<>(result, Duration.between(start, end));
+            long end = System.nanoTime();
+            return new TimeMeasureResult<>(result, Duration.ofNanos(end - start));
         } catch (Exception e) {
-            Instant end = Instant.now();
-            throw new TimeMeasureRuntimeException(e, Duration.between(start, end));
+            long end = System.nanoTime();
+            throw new TimeMeasureRuntimeException(e, Duration.ofNanos(end - start));
         }
     }
 
     public static Duration measure(Runnable task) {
-        Instant start = Instant.now();
+        long start = System.nanoTime();
         try {
             task.run();
-            Instant end = Instant.now();
-            return Duration.between(start, end);
+            long end = System.nanoTime();
+            return Duration.ofNanos(end - start);
         } catch (Exception e) {
-            Instant end = Instant.now();
-            throw new TimeMeasureRuntimeException(e, Duration.between(start, end));
+            long end = System.nanoTime();
+            throw new TimeMeasureRuntimeException(e, Duration.ofNanos(end - start));
         }
     }
 
