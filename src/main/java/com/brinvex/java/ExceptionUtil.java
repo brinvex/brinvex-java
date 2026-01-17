a0d6bea5c3415454b9b2f82ab520ee4adcf185f4
brinvex-dev1 <dev1@brinvex.com>
2026-01-17 18:08:13 +0100

    add ExceptionUtil.getFirstAndSuppressOthers

--- src/main/java/com/brinvex/java/ExceptionUtil.java
+++ src/main/java/com/brinvex/java/ExceptionUtil.java
@@ -1,32 +1,27 @@
 package com.brinvex.java;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.SequencedCollection;
 
 public class ExceptionUtil {
 
-    public static <THROWABLE extends Throwable> THROWABLE getFirstThrowableAndSuppressOthers(List<THROWABLE> exceptions) {
-        int size = exceptions.size();
-        if (size == 0) {
-            throw new IllegalArgumentException("Required non-empty list of exceptions");
-        }
-        THROWABLE mainException = exceptions.getFirst();
-        for (int i = 0; i < size - 1; i++) {
-            mainException.addSuppressed(exceptions.get(i));
+    public static <THROWABLE extends Throwable> THROWABLE getFirstAndSuppressOthers(Collection<THROWABLE> exceptions) {
+        Iterator<THROWABLE> it = exceptions.iterator();
+        THROWABLE mainException = it.next();
+        while (it.hasNext()) {
+            mainException.addSuppressed(it.next());
         }
         return mainException;
     }
 
-    public static <THROWABLE extends Throwable> THROWABLE getLastThrowableAndSuppressOthers(List<THROWABLE> exceptions) {
-        int size = exceptions.size();
-        if (size == 0) {
-            throw new IllegalArgumentException("Required non-empty list of exceptions");
-        }
+    public static <THROWABLE extends Throwable> THROWABLE getLastAndSuppressOthers(SequencedCollection<THROWABLE> exceptions) {
         THROWABLE mainException = exceptions.getLast();
-        for (int i = 0; i < size - 1; i++) {
-            mainException.addSuppressed(exceptions.get(i));
+        for (THROWABLE exception : exceptions) {
+            if (exception != mainException) {
+                mainException.addSuppressed(exception);
+            }
         }
         return mainException;
     }
-
-
 }
