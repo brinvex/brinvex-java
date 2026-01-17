7e3e8fa9b1d7c9c9d98c2e6dbf47aa8f21c6f374
brinvex-dev1 <dev1@brinvex.com>
2026-01-17 14:14:36 +0100

    add ExceptionUtil.getFirstThrowableAndSuppressOthers

--- src/main/java/com/brinvex/java/ExceptionUtil.java
+++ src/main/java/com/brinvex/java/ExceptionUtil.java
@@ -4,19 +4,29 @@ import java.util.List;
 
 public class ExceptionUtil {
 
-    public static <E extends Exception> E getLastExceptionAndSuppressOthers(List<E> exceptions) {
+    public static <THROWABLE extends Throwable> THROWABLE getFirstThrowableAndSuppressOthers(List<THROWABLE> exceptions) {
         int size = exceptions.size();
         if (size == 0) {
             throw new IllegalArgumentException("Required non-empty list of exceptions");
         }
-        if (size == 1) {
-            return exceptions.get(0);
-        }
-        E mainException = exceptions.get(size - 1);
+        THROWABLE mainException = exceptions.getFirst();
         for (int i = 0; i < size - 1; i++) {
             mainException.addSuppressed(exceptions.get(i));
         }
         return mainException;
     }
+
+    public static <THROWABLE extends Throwable> THROWABLE getLastThrowableAndSuppressOthers(List<THROWABLE> exceptions) {
+        int size = exceptions.size();
+        if (size == 0) {
+            throw new IllegalArgumentException("Required non-empty list of exceptions");
+        }
+        THROWABLE mainException = exceptions.getLast();
+        for (int i = 0; i < size - 1; i++) {
+            mainException.addSuppressed(exceptions.get(i));
+        }
+        return mainException;
+    }
+
 
 }
